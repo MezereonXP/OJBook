540c50ba21c662b19dee9bbfb52634099cd44b07
MezereonXP <mezereonxp@gmail.com>
2018-06-26 10:52:06 +0800

    [UPDATE] Add solution linklist 1 in leetcode

---
+++ src/linklist/SolutionLinkList1.java
@@ -0,0 +1,30 @@
+package linklist;
+
+/**
+ * @program: OJBook
+ * @description:
+ * @author: mezereonxp Email: mezereonxp@gmail.com
+ * @create: 2018-06-26 10:07
+ **/
+public class SolutionLinkList1 {
+    public class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+        }
+    }
+
+    public void deleteNode(ListNode node) {
+        int temp = node.next.val;
+        ListNode listNode = node;
+        while (listNode.next.next != null) {
+            listNode.val = temp;
+            listNode = listNode.next;
+            temp = listNode.next.val;
+        }
+        listNode.val = listNode.next.val;
+        listNode.next = null;
+    }
+}
