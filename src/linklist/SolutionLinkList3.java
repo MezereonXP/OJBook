1d56ac26869456eca983e76340dc71d58ec27903
Ke <mezereonxp@gmail.com>
2018-06-30 09:05:30 +0800

    [UPDATE] Add solution linklist 3 for leetcode

---
+++ src/linklist/SolutionLinkList3.java
@@ -0,0 +1,29 @@
+package linklist;
+
+import java.util.List;
+
+/**
+ * @program: SolutionLinkList3
+ * @description: Solution for linklist 3 in leetcode
+ * @author: mezereonxp Email: mezereonxp@gmail.com
+ * @create: 2018/6/26
+ **/
+public class SolutionLinkList3 {
+    public ListNode reverseList(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+        ListNode frontNode = head;
+        ListNode temp = head.next;
+        head.next = null;
+        while (temp != null) {
+            ListNode tempForFront = temp;
+            ListNode newTemp = temp.next;
+            temp.next = frontNode;
+            temp = newTemp;
+            frontNode = tempForFront;
+        }
+        return frontNode;
+    }
+
+}
