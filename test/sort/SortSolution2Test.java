723bc0e4e198b24e353c567abb94c36a1eccc97a
Ke <mezereonxp@gmail.com>
2018-07-06 23:25:34 +0800

    [UPDATE] Add solution sort 2 for leetcode

---
+++ test/sort/SortSolution2Test.java
@@ -0,0 +1,13 @@
+package sort;
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+public class SortSolution2Test {
+
+    @Test
+    public void firstBadVersion() {
+        SortSolution2 sortSolution2 = new SortSolution2();
+        System.out.printf(String.valueOf(sortSolution2.firstBadVersion(5)));
+    }
+}
