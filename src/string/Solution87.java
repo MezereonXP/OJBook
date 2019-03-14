8792b673e210b963b090b81c66e69e69602c9b6f
MezereonXP <mezereonxp@gmail.com>
2019-03-15 00:44:36 +0800

    [UPDATE] Update oj note

--- src/string/Solution87.java
+++ src/string/Solution87.java
@@ -12,8 +12,8 @@ import java.util.Arrays;
 public class Solution87 {
 
     public static void main(String[] args) {
-        String s1 = "great";
-        String s2 = "rgeat";
+        String s1 = "oatzzffqpnwcxhejzjsnpmkmzngneo";
+        String s2 = "acegneonzmkmpnsjzjhxwnpqffzzto";
         System.out.printf(String.valueOf(isScramble(s1, s2)));
     }
 
@@ -21,56 +21,113 @@ public class Solution87 {
         char[] chars1 = s1.toCharArray();
         char[] chars2 = s2.toCharArray();
         int[] history = new int[chars1.length];
+        if (s1.equals(s2))
+            return true;
         if (s1.length() != s2.length())
             return false;
-        for (int i = 0; i < chars1.length; i++) {
-            char temp = chars1[i];
-            if (temp == chars2[i]) {
-                continue;
-            }
-            chars2 = getString(temp, chars2, i, history);
-            if (temp != chars2[i]) {
-                return false;
+        // 从第一个字母选取开始，尝试的进行分割
+        for (int i = 0; i < chars1.length - 1; i++) {
+            if (divide(chars1.clone(), chars2.clone(), i)) {
+                return true;
             }
         }
-        return true;
+        return false;
     }
 
-    public static char[] getString(char s, char[] chars, int position, int[] history) {
-        char[] result = new char[chars.length];
-        for (int i = 0; i < position; i++) {
-            result[i] = chars[i];
+    /**
+     * 尝试性的进行分割，如果左边部分和右边部分都满足是第一个字符数组的排列即可
+     *
+     * @param chars1 第一个字符数组
+     * @param chars2 第二个字符数组
+     * @param i      位置
+     * @return
+     */
+    private static boolean divide(char[] chars1, char[] chars2, int i) {
+        boolean isRight = true;
+        boolean isRight2 = true;
+        boolean tempResult = false;
+        String s11 = "", s12 = "", s21 = "", s22 = "";
+
+        //check正常顺序
+        char[] copy = chars2.clone();
+        for (int j = 0; j < chars1.length; j++) {
+            char tempChar = chars1[j];
+            if (j <= i) {
+                s11 = s11 + chars1[j];
+                s21 = s21 + chars2[j];
+                for (int k = 0; k <= i; k++) {
+                    if (copy[k] == tempChar) {
+                        copy[k] = '*';
+                        break;
+                    }
+                }
+            } else {
+                s12 = s12 + chars1[j];
+                s22 = s22 + chars2[j];
+                for (int k = i + 1; k < chars1.length; k++) {
+                    if (copy[k] == tempChar) {
+                        copy[k] = '*';
+                        break;
+                    }
+                }
+            }
         }
-        int shift = 0, count = 0, flag = 0;
-        for (int i = position; i < chars.length; i++) {
-            if (chars[i] == s) {
-                if (!check(history, position, i))
-                    return chars;
-                result[shift + position] = chars[i];
-                shift = i;
-                flag = 1;
+        for (int j = 0; j < chars1.length; j++) {
+            if (copy[j] != '*') {
+                isRight = false;
                 break;
             }
         }
-        if (flag != 1)
-            return chars;
-        for (int j = shift + 1; j < chars.length; j++) {
-            count++;
-            result[position + count] = chars[j];
+        if (isRight) {
+            tempResult = isScramble(s11, s21) && isScramble(s12, s22);
         }
-        history[position + count] = 1;
-        for (int i = position; i < shift; i++) {
-            result[position + count + 1] = chars[i];
-            count++;
+        //check交换后的顺序
+        copy = chars2.clone();
+        for (int j = i + 1; j < copy.length; j++) {
+            chars2[j - i - 1] = copy[j];
         }
-        return result;
-    }
+        for (int j = 0; j <= i; j++) {
+            chars2[chars1.length - i + j - 1] = copy[j];
+        }
+        copy = chars2.clone();
+        s11 = "";
+        s12 = "";
+        s21 = "";
+        s22 = "";
+        i = chars1.length - i - 2;
 
-    private static boolean check(int[] history, int position, int i) {
-        for (int j = position; j < i; j++) {
-            if (history[j] == 1)
-                return false;
+        for (int j = 0; j < chars1.length; j++) {
+            char tempChar = chars1[j];
+            if (j <= i) {
+                s11 = s11 + chars1[j];
+                s21 = s21 + chars2[j];
+                for (int k = 0; k <= i; k++) {
+                    if (copy[k] == tempChar) {
+                        copy[k] = '*';
+                        break;
+                    }
+                }
+            } else {
+                s12 = s12 + chars1[j];
+                s22 = s22 + chars2[j];
+                for (int k = i + 1; k < chars1.length; k++) {
+                    if (copy[k] == tempChar) {
+                        copy[k] = '*';
+                        break;
+                    }
+                }
+            }
         }
-        return true;
+        for (int j = 0; j <= i; j++) {
+            if (copy[j] != '*') {
+                isRight2 = false;
+                break;
+            }
+        }
+        if (isRight2) {
+            return (isScramble(s11, s21) && isScramble(s12, s22)) || tempResult;
+        } else {
+            return tempResult;
+        }
     }
 }
