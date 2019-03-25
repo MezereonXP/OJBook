5cf4653b299c95f70b3f97fd664078c671770de0
MezereonXP <mezereonxp@gmail.com>
2019-03-25 21:17:19 +0800

    [UPDATE] Update oj note

--- src/others/Solution787.java
+++ src/others/Solution787.java
@@ -1,5 +1,8 @@
 package others;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 /**
  * @ClassName Solution787
  * @Description K 站中转内最便宜的航班
@@ -10,8 +13,8 @@ package others;
 public class Solution787 {
 
     public static void main(String[] args) {
-        int[][] data = {{0, 1, 100}, {1, 2, 100}, {0, 2, 500}};
-        System.out.printf("" + new Solution787().findCheapestPrice(3, data, 0, 2, 1));
+        int[][] data = {{0, 1, 5}, {1, 2, 5}, {0, 3, 2}, {3, 1, 2}, {1, 4, 1}, {4, 2, 1}};
+        System.out.printf("" + new Solution787().findCheapestPrice2(5, data, 0, 2, 2));
     }
 
     public int findCheapestPrice(int n, int[][] flights, int src, int dst, int K) {
@@ -33,6 +36,54 @@ public class Solution787 {
     }
 
     public int findCheapestPrice2(int n, int[][] flights, int src, int dst, int K) {
-        return 0;
+        LinkedList list = new LinkedList();
+
+        int[] min = new int[n];
+        int[] hops = new int[n];
+        int[] visit = new int[n];
+        int count = 0, count2 = 0;
+        int k = 0;
+        // 初始化
+        for (int i = 0; i < flights.length; i++) {
+            if (flights[i][0] == src) {
+                list.addLast(flights[i][1]);
+                min[flights[i][1]] = flights[i][2];
+                hops[flights[i][1]] = 1;
+                count++;
+            }
+        }
+        while (!list.isEmpty()) {
+            if (k >= K)
+                break;
+            int temp = (int) list.getFirst();
+            count--;
+            if (count == 0) {
+                k++;
+                count = count2;
+                count2 = 0;
+            }
+            // 延伸访问节点
+            for (int i = 0; i < flights.length; i++) {
+                if (flights[i][0] == temp) {
+                    if (visit[flights[i][1]] == 0)
+                        list.addLast(flights[i][1]);
+                    if (min[temp] != 0 && min[flights[i][1]] != 0)
+                        if (hops[temp] < K || (hops[temp] == K && flights[i][1] == dst)) {
+                            hops[flights[i][1]] = (min[flights[i][1]] > min[temp] + flights[i][2]) ? hops[temp] + 1 : hops[flights[i][1]];
+                            min[flights[i][1]] = Math.min(min[flights[i][1]], min[temp] + flights[i][2]);
+                        }
+                    if (min[temp] != 0 && min[flights[i][1]] == 0) {
+                        if (hops[temp] < K || (hops[temp] == K && flights[i][1] == dst)) {
+                            min[flights[i][1]] = min[temp] + flights[i][2];
+                            hops[flights[i][1]] = hops[temp] + 1;
+                        }
+                    }
+                    count2++;
+                }
+            }
+            visit[temp] = 1;
+            list.removeFirst();
+        }
+        return min[dst] == 0 ? -1 : min[dst];
     }
 }
